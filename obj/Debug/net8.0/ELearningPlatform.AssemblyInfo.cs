9bc925389e5b550a1e0e389ae12079df9f6daece
Ibrahim <abumusamehibrahim@gmail.com>
2026-06-13 23:31:39 +0400

    Update Bunny Watch and remove Azure code

--- obj/Debug/net8.0/ELearningPlatform.AssemblyInfo.cs
+++ obj/Debug/net8.0/ELearningPlatform.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e998c817e81051e0e3a721c515028e033dc1caa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a932a81d548dba598f30d7b5859b685b1bd8b6c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
