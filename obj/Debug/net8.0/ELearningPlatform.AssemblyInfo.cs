6f5431cb39b88eea46fb67dae4657c80e3605701
Ibrahim <abumusamehibrahim@gmail.com>
2026-09-15 07:28:33 +0400

    Fix Bunny Stream playback with Signed URL

--- obj/Debug/net8.0/ELearningPlatform.AssemblyInfo.cs
+++ obj/Debug/net8.0/ELearningPlatform.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01336e8dd61fff2af8186e954c8b71c269dfa35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b5eb4359630c5f846d0510331b199173f7aa481")]
 [assembly: System.Reflection.AssemblyProductAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
