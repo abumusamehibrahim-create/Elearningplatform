a977718735e250773da804dd3252cc214c402791
Ibrahim <abumusamehibrahim@gmail.com>
2026-09-15 09:40:56 +0400

    Fixc generaurl with Signed URL

--- obj/Debug/net8.0/ELearningPlatform.AssemblyInfo.cs
+++ obj/Debug/net8.0/ELearningPlatform.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36410656af70e8978605ec8e867d51b136448b11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c37187d20a292920138db163af01a429ec2550")]
 [assembly: System.Reflection.AssemblyProductAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
