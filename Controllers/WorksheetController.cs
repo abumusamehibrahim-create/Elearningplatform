4c910ce322c02fe9afcd597d975934c90b974e12
Ibrahim <abumusamehibrahim@gmail.com>
2026-09-18 01:42:09 +0400

    worksheet viewPDF

--- Controllers/WorksheetController.cs
+++ Controllers/WorksheetController.cs
@@ -145,7 +145,7 @@ public class WorksheetController : BaseController
         string url = _bunny.GenerateWorksheetSignedUrl(file.FileName);
 
         ViewBag.PdfUrl = url;
-        return View("ViewPdf", file);
+        return View("ViewPdfBunny", url);
     }
 
 
--- obj/Debug/net8.0/ELearningPlatform.AssemblyInfo.cs
+++ obj/Debug/net8.0/ELearningPlatform.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f1795337c6905f0de9ae388dcd9ab73cb0bb90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e40ac7e625f6923d7603b84959d33ff8c3db222")]
 [assembly: System.Reflection.AssemblyProductAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
