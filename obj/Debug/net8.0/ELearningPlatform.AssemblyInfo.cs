394ef12836b1dd852a09f3f2b67eda701527e60f
Ibrahim <abumusamehibrahim@gmail.com>
2026-09-18 14:20:27 +0400

    update worksheet view

--- obj/Debug/net8.0/ELearningPlatform.AssemblyInfo.cs
+++ obj/Debug/net8.0/ELearningPlatform.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f503496d8922a17718ef88ac61b30f064cfbe14e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a924fb0cb29ee9470a1387104c3fd6ddb27d674")]
 [assembly: System.Reflection.AssemblyProductAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ELearningPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
